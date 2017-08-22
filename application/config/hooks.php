cb77b67fb9f2bd2a6f60b6af05dd508419aa9b16
jssgsy <fcsnwu@163.com>
2017-08-22 21:10:51 +0800

    [CI]完善勾子hook

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -16,6 +16,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * 需要在application/config/config.php中开启hook功能;
  * filepath相对于application目录;
  * 注意param可选，且其类型为mix(可为string,也可为array，也可为其它)
+ *
+ * pre_controller称之为挂钩点，重要的可用值如下：
+ *  pre_system；
+ *  pre_controller: 在你的控制器调用之前执行，所有的基础类都已加载，路由和安全检查也已经完成
+ *  post_controller_constructor: 在你的控制器实例化之后立即执行，控制器的任何方法都还尚未调用
+ *  post_controller: 在你的控制器完全运行结束时执行
+ *  post_system
+ *
+ * 可以一个挂钩点执行多个脚本，如下面的post_controller
+ *
  */
 $hook['pre_controller'] = array(
     'class'    => 'MyHook',
@@ -25,11 +35,17 @@ $hook['pre_controller'] = array(
     'params' => 'pre'
 );
 
-$hook['post_controller'] = array(
+$hook['post_controller'][] = array(
     'class'    => 'MyHook',
     'function' => 'post_controller',
     'filename' => 'MyHook.php',
     'filepath' => 'hooks/univ',
     'params' => array('java', 'php', 'c++')
+);
+$hook['post_controller'][] = array(
+    'class'    => 'MyHook',
+    'function' => 'post_controller2',
+    'filename' => 'MyHook.php',
+    'filepath' => 'hooks/univ'
 );
 
--- application/hooks/univ/MyHook.php
+++ application/hooks/univ/MyHook.php
@@ -5,16 +5,29 @@
  * User: Univ
  * Date: 2017/5/31
  * Time: 23:20
+ *
+ * 参见application/config/hooks.php文件
  */
 class MyHook {
 
+    // 被挂载在pre_controller上
     public function pre_controller($var){
         echo 'this is the hook pre_controler, the $var is ' . $var . '<br>';
     }
 
+    /**
+     * 被挂载在post_controller上
+     * @param $var 配置的参数
+     */
     public function post_controller($var){
-        echo 'this is the hook post_controler, the $var is ';
+        echo 'this is the hook post_controler, the $var is: ' . '<br>';
         print_r($var);
+        echo '<br>';
+    }
+
+    // 被挂载在post_controller上
+    public function post_controller2() {
+        echo 'this is the hook post_controller2, 没有参数 ' . '<br>';
     }
 
 }

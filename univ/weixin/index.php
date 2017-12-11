0cd26bb1ac162ab0e3cca8c4a7b69bbbdd6f0f00
jssgsy <fcsnwu@163.com>
2017-12-11 21:56:29 +0800

    [weixin]回到原点，验证服务器url地址

---
+++ univ/weixin/index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 微信请求的入口，验证自己服务器url的有效性
+ * url:http://45.78.19.224/php_demos/univ/weixin/index.php
+ * User: Univ <minglu.liu@husor.com>
+ * Date: 2017/12/11 19:18
+ * @copyright Beidian Limited. All rights reserved.
+ */
+
+//﻿获得参数 signature nonce token timestamp echostr
+$nonce = $_GET['nonce'];
+$token = 'univ';// 这里微信公众平台后台上自己填的token，两者需要保持一致
+$timestamp = $_GET['timestamp'];
+$echostr = $_GET['echostr'];
+$signature = $_GET['signature'];
+
+//第一步：形成数组，然后按字典序排序
+$tmpArr = array($nonce, $timestamp, $token);
+sort($tmpArr, SORT_STRING);
+
+//第二步：拼接成字符串,sha1加密 ，然后与signature进行校验
+$str = sha1(implode($tmpArr));
+
+//第三步：获得加密后的字符串与signature对比
+if ($str == $signature && $echostr) {
+    //返回给微信服务器
+    echo $echostr;
+    exit;
+}
